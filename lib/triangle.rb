a45978b2d05ef86483c5bde8641d7b34e13e4c55
Vinh Do <vinhdo07@gmail.com>
2019-02-28 01:20:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,13 +3,15 @@ require 'pry'
 class Triangle
   attr_accessor :l1, :l2, :l3, :kind
 
+  class TriangleError < StandardError
+
   def initialize(l1, l2, l3)
     @l1, @l2, @l3 = l1, l2, l3
   end
 
   def kind
     if l1 == 0 || l2 == 0 || l3 == 0
-      
+
     if l1 == l2 && l2 == l3
       @kind = :equilateral
     elsif l1 == l2 || l2 == l3 || l1 == l3
