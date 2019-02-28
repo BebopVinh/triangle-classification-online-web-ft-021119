cff2ba716fc2175a6d549c1cd0cbe786257da031
Vinh Do <vinhdo07@gmail.com>
2019-02-28 01:21:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,8 +12,8 @@ class Triangle
 
   def kind
     if l1 == 0 || l2 == 0 || l3 == 0
-
-    if l1 == l2 && l2 == l3
+      raise TriangleError
+    elsif l1 == l2 && l2 == l3
       @kind = :equilateral
     elsif l1 == l2 || l2 == l3 || l1 == l3
       @kind = :isosceles
