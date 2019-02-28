3927d44297f13933e6e92f4de7b208b0418bf564
Vinh Do <vinhdo07@gmail.com>
2019-02-28 01:16:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,15 @@
 require 'pry'
 
 class Triangle
-  attr_accessor :l1, :l2, :l3
+  attr_accessor :l1, :l2, :l3, :kind
 
   def initialize(l1, l2, l3)
     @l1, @l2, @l3 = l1, l2, l3
   end
-end
+
+  def kind
+    if l1 == l2 == l3
+      @kind = ':equilateral'
+    end
+  end
+end #End of Triangle Class
