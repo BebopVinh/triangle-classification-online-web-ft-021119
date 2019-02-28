fbae76bd18e8e3a08984f455ff2bfa8d9bcd12ca
Vinh Do <vinhdo07@gmail.com>
2019-02-28 01:29:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,14 +14,14 @@ class Triangle
     array = [@l1, @l2, @l3]
     if array.any?{|l| l == 0}
       raise TriangleError
-    elsif array.find {|a,b,c| a + b > c}
-      raise TriangleError
     elsif l1 == l2 && l2 == l3
       @kind = :equilateral
     elsif l1 == l2 || l2 == l3 || l1 == l3
       @kind = :isosceles
-    else
+    elsif !(l1 == l2 && l2 == l3)
       @kind = :scalene
+    else
+      raise TriangleError
     end
   end
 end #End of Triangle Class
